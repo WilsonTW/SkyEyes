93645a9ca2a47daf98a4b3158f1fa2ad63515322
WilsonTW <sogoph87@gmail.com>
2018-03-30 08:42:02 +0800

    remark unused parameters

--- app/src/main/java/com/app/androidkt/mqtt/Constants.java
+++ app/src/main/java/com/app/androidkt/mqtt/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 
     public static final String MQTT_BROKER_URL = "tcp://172.20.10.21:1883";
 
-    public static final String PUBLISH_TOPIC = "WKS/SkyEyes/StationUser/Notification";
+//    public static final String PUBLISH_TOPIC = "WKS/SkyEyes/StationUser/Notification";
 
     public static final String PUBLISH_TOPIC_CMD = "WKS/SkyEyes/UAV_AgentService/Receiver";
 
